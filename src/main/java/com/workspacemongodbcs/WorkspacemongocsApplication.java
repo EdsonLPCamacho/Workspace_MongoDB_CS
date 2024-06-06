38baeb7519d8cacabe79aa4f938de7d78d635eab
Edson Camacho <edsonlpcamacho@gmail.com>
2024-06-06 09:38:19 -0300

    Project Started

---
+++ src/main/java/com/workspacemongodbcs/WorkspacemongocsApplication.java
@@ -0,0 +1,13 @@
+package com.workspacemongodbcs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkspacemongocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkspacemongocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workspacemongodbcs/WorkspacemongocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workspacemongodbcs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkspacemongocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
